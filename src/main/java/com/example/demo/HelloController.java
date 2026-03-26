c03c41165d80e2617d0e9bbe008110430d545f2c
arshimathur <arshimathur@gmail.com>
2026-03-27 02:26:02 +0530

    Fix return statement in home method

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "return "Updated by new developer";
+        return "Updated by new developer";
     }
 }
