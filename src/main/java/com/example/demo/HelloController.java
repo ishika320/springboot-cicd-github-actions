8a04362e9a0b49e45fed7f9654d56e842cf429f6
Ishika <ishikagujjar21@gmail.com>
2026-04-02 20:43:48 +0530

    Change endpoint mapping to '/message'

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/ message")
     public String home() {
         return "Updated by new developer";
     }
